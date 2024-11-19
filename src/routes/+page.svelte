da2699c5eb0af0f43f8459497ac5cb5594d0b05a
Utkub24 <utkubicer24@gmail.com>
2024-11-20 00:29:38 +0300

    feat: setup emulator w/ dummy UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
   import { initializeApp } from "firebase/app";
-  import { getAuth } from "firebase/auth";
+  import {
+    getAuth,
+    connectAuthEmulator,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+  } from "firebase/auth";
+
+  let emailInput: string;
+  let passwordInput: string;
 
   const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,6 +21,65 @@
 
   const firebaseApp = initializeApp(firebaseConfig);
   const auth = getAuth(firebaseApp);
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+
+  const register = () => {
+    createUserWithEmailAndPassword(auth, emailInput, passwordInput)
+      .then((userCredential) => {
+        // signed up
+        const user = userCredential.user;
+        console.log("User registered");
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(`Error: ${errorCode}\n${errorMessage}`);
+      });
+  };
+
+  const signIn = () => {
+    signInWithEmailAndPassword(auth, emailInput, passwordInput)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(`User signed in`);
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(`Error: ${errorCode}\n${errorMessage}`);
+      });
+  };
 </script>
 
-<h1>Welcome to Witch Puzzles Dummy Auth UI!</h1>
+<div class="vertical-flex">
+  <h1>Welcome to Witch Puzzles Dummy Auth UI!</h1>
+  <div class="vertical-flex">
+    <input type="text" placeholder="Email" bind:value={emailInput} />
+    <input type="text" placeholder="Password" bind:value={passwordInput} />
+  </div>
+  <div>
+    <button on:click={register}>Register</button>
+    <button on:click={signIn}>Sign in</button>
+  </div>
+</div>
+
+<style lang="postcss">
+  h1 {
+    @apply font-extrabold text-2xl;
+  }
+
+  button {
+    @apply p-1 text-white bg-orange-500 border-2 border-black;
+  }
+
+  input {
+    @apply p-1 border-2 border-black;
+  }
+
+  .vertical-flex {
+    @apply flex flex-col items-center gap-2;
+  }
+</style>
