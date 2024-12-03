eb7b2e612855006df624dcd7fb51f7b2151102b0
Utkub24 <utkubicer24@gmail.com>
2024-12-03 20:27:49 +0300

    feat: add analytics id env variable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,11 +19,16 @@
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
   };
 
   const firebaseApp = initializeApp(firebaseConfig);
   const auth = getAuth(firebaseApp);
-  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+
+  const authEmulatorPort = import.meta.env.VITE_AUTH_EMULATOR_PORT;
+
+  // comment out the line below to connect to the real service
+  connectAuthEmulator(auth, `http://127.0.0.1:${authEmulatorPort}`);
 
   const setStatusSuccessStyle = () => {
     statusElement.classList.remove("text-red-700");
