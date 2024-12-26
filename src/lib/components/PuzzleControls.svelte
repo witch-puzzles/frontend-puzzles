22ba242dec0437e18a6ece705cb7135c92137716
Utkub24 <utkubicer24@gmail.com>
2024-12-27 01:11:28 +0300

    feat: tip and puzzle controls

---
+++ src/lib/components/PuzzleControls.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Icon from "$lib/components/Icon.svelte";
+  import Button, { Label } from "@smui/button";
+
+  interface Props {
+    time?: string;
+    submit: () => void;
+    reset: () => void;
+    shuffle: () => void;
+  }
+
+  let { time = $bindable("00:57"), submit, reset, shuffle }: Props = $props();
+</script>
+
+<div class="flex flex-col items-center border border-black">
+  <div class="flex flex-row items-center gap-2 bg-blue-100">
+    <Icon tag="clock" class="w-12 h-12" />
+    {time}
+  </div>
+  <div class="flex flex-row justify-around gap-2 m-2">
+    <Button onclick={() => submit()} variant="raised"
+      ><Label>Submit</Label></Button
+    >
+    <Button onclick={() => reset()}><Label>Clear</Label></Button>
+    <button onclick={() => shuffle()} class="border border-black"
+      ><Icon tag="shuffle" class="w-6 h-6" /></button
+    >
+  </div>
+</div>
--- src/routes/puzzle/sudoku/+page.svelte
+++ src/routes/puzzle/sudoku/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
   import SudokuGrid from "$lib/components/SudokuGrid.svelte";
+  import PuzzleControls from "$lib/components/PuzzleControls.svelte";
+
+  const submit = () => {
+    console.log("Submit puzzle...");
+  };
+
+  const reset = () => {
+    console.log("Reset puzzle...");
+  };
+
+  const shuffle = () => {
+    console.log("Shuffle puzzle...");
+  };
 </script>
 
-<div class="flex flex-col items-center">
-  <SudokuGrid size={3} />
+<div class="flex flex-row items-center justify-center gap-12">
+  <div class="flex flex-col items-center">
+    <h6 class="mdc-typography--headline6">How to Play</h6>
+    <p>Lorem ipsum dolor sit amet</p>
+  </div>
+  <div class="flex flex-col items-center">
+    <h3 class="mdc-typography--headline3">Sudoku</h3>
+    <SudokuGrid size={3} />
+  </div>
+  <PuzzleControls {submit} {reset} {shuffle} />
 </div>
