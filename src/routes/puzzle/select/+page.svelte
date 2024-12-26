f728f541c9bbff21adfb510d1b991bac363b9c0f
Utkub24 <utkubicer24@gmail.com>
2024-12-26 20:44:08 +0300

    feat: route to `/puzzle/name` on click

--- src/routes/puzzle/select/+page.svelte
+++ src/routes/puzzle/select/+page.svelte
@@ -3,8 +3,11 @@
   import Button, { Label } from "@smui/button";
   import { PuzzleDifficulty } from "$lib/Puzzle";
   import SudokuPuzzle from "$lib/SudokuPuzzle";
+  import type Puzzle from "$lib/Puzzle";
 
   const puzzles = [new SudokuPuzzle(), new SudokuPuzzle(), new SudokuPuzzle()];
+
+  let selectedPuzzle: Puzzle = $state(puzzles[0]);
 
   let selectedDifficulty: PuzzleDifficulty = $state(
     puzzles[0].availableDifficulties[0],
@@ -13,6 +16,9 @@
 
 <div class="flex flex-col items-center gap-8">
   <h1 class="mdc-typography--headline3">Which puzzle do you want to solve?</h1>
-  <PuzzleSelector {puzzles} bind:selectedDifficulty />
-  <Button variant="raised"><Label>Solve!</Label></Button>
+  <PuzzleSelector {puzzles} bind:selectedPuzzle bind:selectedDifficulty />
+  <!-- TODO: add query params for difficulty etc. -->
+  <a href={`/puzzle/${selectedPuzzle.name.toLowerCase()}`}>
+    <Button variant="raised"><Label>Solve!</Label></Button>
+  </a>
 </div>
