ab3366338362d64c8cc7fda0d0e8d9f2cdf16084
Erthium <ertyum35@gmail.com>
2025-01-03 08:03:19 +0300

    feat: add auth headers to leaderboard service

--- src/lib/LeaderboardService.ts
+++ src/lib/LeaderboardService.ts
@@ -1,4 +1,5 @@
 import { LeaderboardDto, LeaderboardRegistryDto } from "./dto/Leaderboard.dto";
+import { firebaseService } from "./FirebaseService";
 
 export default class LeaderboardService {
 
@@ -8,30 +9,49 @@ export default class LeaderboardService {
     this.baseUrl = import.meta.env.VITE_BACKEND_URL;
   }
 
+  async getHeaders(): Promise<Headers> {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    try {
+      const accessToken = await firebaseService.getIdToken();
+      if (accessToken) {
+        headers.append("Authorization", "Bearer " + accessToken);
+      }
+    }
+    catch (e) {
+      console.error(e);
+    }
+    return headers;
+  }
+
   async getLeaderboardToday(difficulty: number): Promise<LeaderboardDto> {
+    const headers = await this.getHeaders();
     const url = `${this.baseUrl}/v1/sudoku_registry/leaderboard/${difficulty}/today`;
-    const response = await fetch(url);
+    const response = await fetch(url, { headers });
     const responseData = await response.json();
     return responseData as LeaderboardDto;
   }
 
   async getLeaderboardWeek(difficulty: number): Promise<LeaderboardDto> {
-    const url = `${this.baseUrl}/v1/sudoku_registry/leaderboard/${difficulty}/week`;
-    const response = await fetch(url);
+    const headers = await this.getHeaders();
+    const url = `${this.baseUrl}/v1/sudoku_registry/leaderboard/${difficulty}/week`;    
+    const response = await fetch(url, { headers});
     const responseData = await response.json();
     return responseData as LeaderboardDto;
   }
 
   async getLeaderboardMonth(difficulty: number): Promise<LeaderboardDto> {
+    const headers = await this.getHeaders();
     const url = `${this.baseUrl}/v1/sudoku_registry/leaderboard/${difficulty}/month`;
-    const response = await fetch(url);
+    const response = await fetch(url, { headers });
     const responseData = await response.json();
     return responseData as LeaderboardDto;
   }
 
   async getLeaderboardAllTime(difficulty: number): Promise<LeaderboardDto> {
+    const headers = await this.getHeaders();
     const url = `${this.baseUrl}/v1/sudoku_registry/leaderboard/${difficulty}/alltime`;
-    const response = await fetch(url);
+    const response = await fetch(url, { headers });
     const responseData = await response.json();
     return responseData as LeaderboardDto;
   }
